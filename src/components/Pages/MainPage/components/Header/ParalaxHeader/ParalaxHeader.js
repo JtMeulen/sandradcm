8f5c25c82ae4a309f1968bdae3af172297fa70ab
Joey ter Meulen <joeytermeulen@gmail.com>
2019-11-30 23:25:55 +0100

    get paralax from google

--- src/components/Pages/MainPage/components/Header/ParalaxHeader/ParalaxHeader.js
+++ src/components/Pages/MainPage/components/Header/ParalaxHeader/ParalaxHeader.js
@@ -2,20 +2,28 @@ import React, { Fragment } from 'react';
 
 import styles from './ParalaxHeader.module.css';
 
+const PARALAX_URL = [
+  '1ij7FsjfufWuOQ2RiA1YbcluDFYHs9UbN',
+  '1-YAfMlZiX8iXL6J_7KSY7qBS7hjVXY7W',
+  '1BI3fGbI_cx4zGny-8Fpg7cRliZ_4ANpk',
+  '1LGAPowQHxGwg5fIlrs3wGweQDL_owGzj',
+  '1ORNPFRcnxrVtQ8BeH8ja3ki039zlMKBb'
+]
+
 const ParalaxHeader = ({ headerScrollPercent }) => {
   return (
     <Fragment>
       <div
         className={styles.paralax}
         style={{
-          backgroundImage: 'url("http://www.firewatchgame.com/images/parallax/parallax0@2x.png")',
+          backgroundImage: `url('https://drive.google.com/uc?id=${PARALAX_URL[0]}')`,
           position: 'fixed'
         }}
       />
       <div
         className={styles.paralax}
         style={{
-          backgroundImage: 'url("http://www.firewatchgame.com/images/parallax/parallax1@2x.png")',
+          backgroundImage: `url('https://drive.google.com/uc?id=${PARALAX_URL[1]}')`,
           position: 'fixed',
           top: `-${headerScrollPercent * 1}px`
         }}
@@ -23,7 +31,7 @@ const ParalaxHeader = ({ headerScrollPercent }) => {
       <div
         className={styles.paralax}
         style={{
-          backgroundImage: 'url("http://www.firewatchgame.com/images/parallax/parallax3@2x.png")',
+          backgroundImage: `url('https://drive.google.com/uc?id=${PARALAX_URL[2]}')`,
           position: 'fixed',
           top: `-${headerScrollPercent * 2}px`
         }}
@@ -31,7 +39,7 @@ const ParalaxHeader = ({ headerScrollPercent }) => {
       <div
         className={styles.paralax}
         style={{
-          backgroundImage: 'url("http://www.firewatchgame.com/images/parallax/parallax5@2x.png")',
+          backgroundImage: `url('https://drive.google.com/uc?id=${PARALAX_URL[3]}')`,
           position: 'fixed',
           top: `-${headerScrollPercent * 5}px`
         }}
@@ -39,7 +47,7 @@ const ParalaxHeader = ({ headerScrollPercent }) => {
       <div
         className={styles.paralax}
         style={{
-          backgroundImage: 'url("http://www.firewatchgame.com/images/parallax/parallax7@2x.png")'
+          backgroundImage: `url('https://drive.google.com/uc?id=${PARALAX_URL[4]}')`
         }}
       />
     </Fragment>
