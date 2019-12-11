f0a2aea14257976ff2e0177280597a1adf94ee53
Joey ter Meulen <joeytermeulen@gmail.com>
2019-12-11 10:53:17 +0100

    add real thumbnails

--- src/components/Pages/GamePage/pages/components/CandyCrushSaga.js
+++ src/components/Pages/GamePage/pages/components/CandyCrushSaga.js
@@ -25,16 +25,40 @@ const CandyCrushSaga = (props) => {
   ];
 
   const thumbnails = [
-    '1_aY98Lk6FHivqEKBSv92O42_9QT4LgGT',
-    '1_aY98Lk6FHivqEKBSv92O42_9QT4LgGT',
-    '1_aY98Lk6FHivqEKBSv92O42_9QT4LgGT',
-    '1_aY98Lk6FHivqEKBSv92O42_9QT4LgGT',
-    '1_aY98Lk6FHivqEKBSv92O42_9QT4LgGT',
-    '1_aY98Lk6FHivqEKBSv92O42_9QT4LgGT',
-    '1_aY98Lk6FHivqEKBSv92O42_9QT4LgGT',
-    '1_aY98Lk6FHivqEKBSv92O42_9QT4LgGT',
-    '1w6zJtElrKhc1Bo9IYp6MmCRruYs9A3eT'
-  ];
+		'17QYS84EvsMaozZ4xzA7hJFHxmzYUzZWo',
+		'1u-Zt_RoZChz-OvGHFHzJbfAMH5Wgt1dO',
+		'1bY4ba3_M2zUwwRXXljejB3VqDRC4lYMT',
+		'1kN-9r1NqdmpCoz4cdf9qLROwNwO8UAGi',
+		'1RqZVZeKhO0fP2JNJy4ubMhSILrR651v2',
+		'1Y6_NM9Wd1sIjznW3S2y8qHMTRc4kKLPT',
+		'1M0xvyH3lnIrYbNPmpBFplUNLkbRfzwVY',
+		'17RlxeBzcGwFItzy3tkkXgE_WB72_XEMF',
+		'11e-o-63gsk0lQiZ8UWwKrqtV3dPtB-1u',
+		'1xRLIWe8CwkF5c9P2OzLd1rQ2bVLw6U7o',
+		'12lQe0zXZ6gTDa-gCiH8yzwLPgS8nqCa2',
+		'17qvOlOFDn0VtBncPrkdKLaqIlBTDkkhs',
+		'1ZXIRDEdVKLI5UpJ73WffCWPC_1vwXDGb',
+		'19OR-EYoXo_OtF4iWxww9buq_4kh7kg10',
+		'1XW7E2xOSnTLV_cdNcn92HgJLRE9zKhfm',
+		'1o7d70laeEp5wkh5Ds8NPm6Y69QWY_JM8',
+		'19fZiFXHYAns6UdRGSMouiBqin15gB-Xz',
+		'1VrCzxDssHGHqMN8DWKGvC2CJuz2ngvuB',
+		'17lSLJR41RiZwgATLpQXTv7cM3XAvN_tI',
+		'1I_vJBcx3f6DR_B0bjpX9-N38UtNvYExV',
+		'1_H2IaOrsVHsa0tB9m6y0rMMO67XSB4Uk',
+		'1pLYFj-dqDaM6gXxrAwh_v4YSaR1ecHUG',
+		'1IKzRVLK4_0aq-43fYP9-MXZQ1vI6kKaV',
+		'1YqL8ApmMLISAfmU-nq6Fw_ZiIpCJ3azm',
+		'1PN1e4m8nN6kbhyND--WfieEZqKfh8h2P',
+		'1VBazvL2P7ChoAekHmPpEoREoJUWXmSvX',
+		'1M4Mbyxu475whkVxYqmJaIf_01KYI3NWL',
+		'1kTY3LnhRkwQIadxcwyv16kkm-HCK4P7m',
+		'1Nfy0gwZEbvdNWbU8jc7yQ4E8o1O-ZIf-',
+		'11iXMSTpEImCC-MDSAXyGfbjQY8DGCMZG',
+		'14GFsTIy-0JhJZpdNCpTA8QOKfwC-M5tQ',
+		'1PLq_nIxmR-r7iUtK5nL8-Gaf2RivSMbV',
+		'1Y_DbflSYsM0Nk7OD-Fi8PlNS3iV318n9'
+	];
 
   const BBC_URL = <a href="https://www.bbc.com/news/technology-37484114" target="_blank" rel="noopener noreferrer">the BBC</a>;
 
@@ -83,7 +107,7 @@ const CandyCrushSaga = (props) => {
 				depict main characters Tiffi and Yeti in search what would happen after level 2000. The
 				Bubblegum Troll was there to soothe their Y2K scare with a glimpse into the future.
 				</p>
-				<Thumbnails thumbnails={thumbnails} positions={[6,7]}/>
+				<Thumbnails thumbnails={thumbnails} positions={[6,7,8,9,10,11]}/>
 				<p>
 				My designs were featured on worldwide media (such as {BBC_URL}),
 				in app stores and on social media. King celebrated this with a big campaign – check out the video below!
@@ -94,7 +118,7 @@ const CandyCrushSaga = (props) => {
 				character Tiffi out hunting for potion ingredients at Halloween 2015. After a player finished a
 				special set of levels, Caramella would transform in happiness and hand out her treats.
 				</p>
-				<Thumbnails thumbnails={thumbnails} positions={[8]}/>
+				<Thumbnails thumbnails={thumbnails} positions={[12,13,14,15,16,17]}/>
 				<p>
 				Other examples are the snowmen Holly and Dale, who often are used in holiday and winter
 				themed episodes and live-ops.
@@ -105,7 +129,7 @@ const CandyCrushSaga = (props) => {
 				visually and with technical implementation, but also enjoying partaking in designing new
 				features with the team.
 				</p>
-				{/* TODO: CONTAINER WITH THUMBNAILS */}
+				<Thumbnails thumbnails={thumbnails} positions={[18,19,20,21,22,23]}/>
 				<p>
 				Candy’s first collaboration with an outside brand was on a live ops with the US based Marie
 				Claire magazine. Then Creative Director Nina Garcia designed a Candy level for a spread they did
@@ -121,6 +145,7 @@ const CandyCrushSaga = (props) => {
 				portraits of players and colleagues and more. Some of my designs were also featured in the
 				Candy Crush TV show.
 				</p>
+				<Thumbnails thumbnails={thumbnails} positions={[24,25,26,27,28,29,30,31,32]}/>
 			</div>
 		</Fragment>
 		)
