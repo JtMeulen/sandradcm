0ef6359b39b566afc58480322846125930f61877
JoeyM <joeytermeulen@gmail.com>
2020-02-02 14:30:36 +0100

    added youtube thumbs

--- src/components/Pages/GamePage/pages/components/CandyCrushSaga.js
+++ src/components/Pages/GamePage/pages/components/CandyCrushSaga.js
@@ -55,36 +55,37 @@ const CandyCrushSaga = (props) => {
 		{image: '1r1YgRktTWdAIlbHSa3zB9DtbHK7V1Mrw', text: "Design for Mr. Snappy's Garden"}, //4 CCS_SnappysGarden2
 		{image: '1Ua6wFDyi1Q5XalOyBR5Z_tyPcKcks6rt', text: ''}, //5 CCS_SnappysGarden3
 		{image: '1SXbIVOORTr0cEe7fBdG33wO9Ay_L2MGH', text: 'Didi the dragon', options: 'whiteBG'}, //6 Didi
-		{image: '1xGSHFkDQFpTMKoHy6ugKN8TYqIfx4p35', text: 'Didi featured a lot across all types of media'}, //7 CandyPet_Featuring
-		{image: '1-DmS4T4Y0lo_E7hZkGY0YBDvcRCkiDIl', text: 'Rami the sheep and my remake of existing character Ramsey, on whom Rami was based', options: 'isMedium' }, //8 CandyPet_Rami
-		{image: '1aqAdhD0XZiCUMXxLT2Lw280vV9AiqfkW', text: "New Year's 2015-2016"}, //9 LiveOps_NY2016
-		{image: '1ESIefIwsO-BeQAjd4LygQx_Km_QE3tiV', text: "Yeti's map treasure hunt"}, //10 LiveOps_Keys
-		{image: '1edi684aIhpwKzKSl1Jt7nNxe2NM4_Hx1', text: 'Halloween 2017'}, //11 LiveOps_Spooky
-		{image: '1aoX-_eAYLRZY9hB9QPsySBf55Sl03KTO', text: 'Crispy Clearing'}, //12 EP1
-		{image: '1RAeVOhoDtv3OTJ_ns4mV__QILSdyPKbc', text: 'Treacle Retreat'}, //13 EP2
-		{image: '1RhkKcagVgcqH-Tjtg1fguLihN8KYdh5r', text: 'Cobweb Canopy'}, //14 EP3
-		{image: '1SJaAGDk967J1AY9oqnIGnq56HkOqgd2K', text: 'Swirly Steppes'}, //15 EP4
-		{image: '1X9Q5Cf1TBIYoICo_VXoamLV7sIPfnDah', text: 'Smiley Seas'}, //16 EP5
-		{image: '1TpB-z5nkl20HcMjfnVuuY2N69whXGxeQ', text: 'Popsicle Plateau'}, //17 EP6
-		{image: '15FpN-a4-A2AcqzEiJWiUCBk12r2wfOta', text: 'Icing Islands'}, //18 EP7
-		{image: '1ayWJJbARzuASCtqWfr4NHJUTjkML1x4Z', text: 'Pastry Peaks'}, //19 EP8
-		{image: '1KtrOuKqV77e58Ax4BDC8NH_5OullTnoq', text: 'Delicate Dale'}, //20 EP9
-		{image: '1IO9DhVrysOaq9UwzlTPGohybQsOPcOnd', text: 'Candied Cluster'}, //21 3DEP1
-		{image: '1qooOXjv8sR1jFKOBqrPrX99Kh5ReXTHw', text: 'Sweet Stadium'}, //22 3DEP2
-		{image: '146HFhfHo6FHwqdqd7mXxfPZAHCYQNBxC', text: 'Popsicle'}, //23 3DEP3
-		{image: '1EljKggq42yR5_14kcr2s_rV3dR8xOrlN', text: 'Lovely Lagoon'}, //24 3DEP4
-		{image: '1CE4ClDD6yEiarr-3czBvYgPObSnCwpnH', text: 'Honey Hives'}, //25 3DEP6
-		{image: '1c3v0H-QuULayxd3_g7g7bLFykY1I4S4W', text: 'Breezy Bedroom'}, //26 3DEP5
-		{image: '1J1y9Q-Tk-hAkBmQpCeoiU58hQqPk-JyP', text: 'Orderly Office'}, //27 3DEP7
-		{image: '1gp_sIg7iYet-3MSzHUan7dYIC9z8urlk', text: 'Licorice Lane'}, //28 3DEP8
-		{image: '15Iv4hXylUb47WV8IDEAILC3WVd83OSV_', text: 'Glittery Gala'}, //29 3DEP9
-		{image: '1DX1WnGmEhO9aEWwAtSw-QKAZORymu3N1', text: 'Lucky Lawn'}, //30 3DEP10
-		{image: '1DRHQpot7lH4BXjibzHdDqRKO1-jc7XVh', text: 'Munchy Meteor'}, //31 3DEP11
-		{image: '1u0oCDCU89-zBoHzbqMXzjdMJb46pbf9u', text: 'Bounty Bush'}, //32 3DEP12
-		{image: '1J8LYXYOAMy7PpRQBTsaAFnYRFBvI17ah', text: ''}, //33 BubblegumBazaar1
-		{image: '1GSAWRTS4c3u9z7Ti1X3OmEmCUAerct_-', text: ''}, //34 BubblegumBazaar1
-		{image: '1_lDnWgHihN7nIUxyeoIrF_I-HF_Bt2Ge', text: ''}, //35 BubblegumBazaar1
-		{image: '1yZKbBVAxfDMvY5YHh-LQmUL8vgC2Ejzm', text: ''} //36 Placeholder
+		{image: '1-SQnVu1RhwijzPjxEIjmLR2oPAEGjbuT', text: 'Collect candies in-game to bake treats'}, //7 CandyPet_Screenshot
+		{image: '1xGSHFkDQFpTMKoHy6ugKN8TYqIfx4p35', text: 'Didi featured a lot across all types of media'}, //8 CandyPet_Featuring
+		{image: '1-DmS4T4Y0lo_E7hZkGY0YBDvcRCkiDIl', text: 'Pet Rami, based on my remake of existing character Ramsey', options: 'isMedium' }, //9 CandyPet_Rami
+		{image: '1aqAdhD0XZiCUMXxLT2Lw280vV9AiqfkW', text: "New Year's 2015-2016"}, //10 LiveOps_NY2016
+		{image: '1ESIefIwsO-BeQAjd4LygQx_Km_QE3tiV', text: "Yeti's map treasure hunt"}, //11 LiveOps_Keys
+		{image: '1edi684aIhpwKzKSl1Jt7nNxe2NM4_Hx1', text: 'Halloween 2017'}, //12 LiveOps_Spooky
+		{image: '1aoX-_eAYLRZY9hB9QPsySBf55Sl03KTO', text: 'Crispy Clearing'}, //13 EP1
+		{image: '1RAeVOhoDtv3OTJ_ns4mV__QILSdyPKbc', text: 'Treacle Retreat'}, //14 EP2
+		{image: '1RhkKcagVgcqH-Tjtg1fguLihN8KYdh5r', text: 'Cobweb Canopy'}, //15 EP3
+		{image: '1SJaAGDk967J1AY9oqnIGnq56HkOqgd2K', text: 'Swirly Steppes'}, //16 EP4
+		{image: '1X9Q5Cf1TBIYoICo_VXoamLV7sIPfnDah', text: 'Smiley Seas'}, //17 EP5
+		{image: '1TpB-z5nkl20HcMjfnVuuY2N69whXGxeQ', text: 'Popsicle Plateau'}, //18 EP6
+		{image: '15FpN-a4-A2AcqzEiJWiUCBk12r2wfOta', text: 'Icing Islands'}, //19 EP7
+		{image: '1ayWJJbARzuASCtqWfr4NHJUTjkML1x4Z', text: 'Pastry Peaks'}, //20 EP8
+		{image: '1KtrOuKqV77e58Ax4BDC8NH_5OullTnoq', text: 'Delicate Dale'}, //21 EP9
+		{image: '1IO9DhVrysOaq9UwzlTPGohybQsOPcOnd', text: 'Candied Cluster'}, //22 3DEP1
+		{image: '1qooOXjv8sR1jFKOBqrPrX99Kh5ReXTHw', text: 'Sweet Stadium'}, //23 3DEP2
+		{image: '146HFhfHo6FHwqdqd7mXxfPZAHCYQNBxC', text: 'Popsicle'}, //24 3DEP3
+		{image: '1EljKggq42yR5_14kcr2s_rV3dR8xOrlN', text: 'Lovely Lagoon'}, //25 3DEP4
+		{image: '1CE4ClDD6yEiarr-3czBvYgPObSnCwpnH', text: 'Honey Hives'}, //26 3DEP6
+		{image: '1c3v0H-QuULayxd3_g7g7bLFykY1I4S4W', text: 'Breezy Bedroom'}, //27 3DEP5
+		{image: '1J1y9Q-Tk-hAkBmQpCeoiU58hQqPk-JyP', text: 'Orderly Office'}, //28 3DEP7
+		{image: '1gp_sIg7iYet-3MSzHUan7dYIC9z8urlk', text: 'Licorice Lane'}, //29 3DEP8
+		{image: '15Iv4hXylUb47WV8IDEAILC3WVd83OSV_', text: 'Glittery Gala'}, //30 3DEP9
+		{image: '1DX1WnGmEhO9aEWwAtSw-QKAZORymu3N1', text: 'Lucky Lawn'}, //31 3DEP10
+		{image: '1DRHQpot7lH4BXjibzHdDqRKO1-jc7XVh', text: 'Munchy Meteor'}, //32 3DEP11
+		{image: '1u0oCDCU89-zBoHzbqMXzjdMJb46pbf9u', text: 'Bounty Bush'}, //33 3DEP12
+		{image: '1J8LYXYOAMy7PpRQBTsaAFnYRFBvI17ah', text: ''}, //34 BubblegumBazaar1
+		{image: '1GSAWRTS4c3u9z7Ti1X3OmEmCUAerct_-', text: ''}, //35 BubblegumBazaar1
+		{image: '1_lDnWgHihN7nIUxyeoIrF_I-HF_Bt2Ge', text: ''}, //36 BubblegumBazaar1
+		{image: '1yZKbBVAxfDMvY5YHh-LQmUL8vgC2Ejzm', text: ''} //37 Placeholder
 	];
 
   const BBC_URL = <a href="https://www.bbc.com/news/technology-37484114" target="_blank" rel="noopener noreferrer">the BBC</a>;
@@ -121,7 +122,7 @@ const CandyCrushSaga = (props) => {
 				from composing images to implementing and animating sprites in a 3D environment via King's in-house editor, which is
 				comparable to Unity. 
 				</p>
-				<Thumbnails thumbnails={thumbnails} positions={[36,36,36]} height={248} marginBottom={0} />
+				<Thumbnails thumbnails={thumbnails} positions={[37,37,37]} height={248} marginBottom={0} />
 				<p>
 				I have contributed actively to the game design of new features, designed UI elements like pop-ups
 				and oftend supported the narrative and localization crafts with writing and organizing copy.
@@ -137,7 +138,7 @@ const CandyCrushSaga = (props) => {
 				When the Candy Pet feature is active, playing levels provides candy to bake into snacks for your little hungry friend. {DIDI_URL}<br /><br />
 				I got to work on both the game design and art for Candy Pet, trying to design baby versions of existing <em>Candy</em> characters - it was a blast.
 				</p>
-				<Thumbnails thumbnails={thumbnails} positions={[7,8]} height={492} marginBottom={50} />
+				<Thumbnails thumbnails={thumbnails} positions={[7,8,9]} height={325} marginBottom={50} />
 				<p>
 				Another character I created was the witch Caramella, who first sent main character Tiffi out hunting for potion ingredients at Halloween 2015. After a player finished a
 				special set of levels, Caramella would transform in happiness and hand out her treats.
@@ -154,15 +155,15 @@ const CandyCrushSaga = (props) => {
 				When working with these features, I contributed visually, with game design suggestions and with technical implementation. Sometimes I simply reskinned existing
 				features as well.
 				</p>
-				<Thumbnails thumbnails={thumbnails} positions={[9,10,11]} height={374} marginBottom={33} />
+				<Thumbnails thumbnails={thumbnails} positions={[10,11,12]} height={374} marginBottom={33} />
 				<p>
 				One of my biggest occupations within <em>Candy</em>, however, has been involvement with the Saga map format and its episodes. <br /><br />
 				Episodes are bundles of 15 new levels, released every week, with custom art and narrative on the Saga map and behind the game board. 
 				Here are some examples of mine!
 				</p>
-				<Thumbnails thumbnails={thumbnails} positions={[12,13,14]} height={248} marginBottom={33} />
-				<Thumbnails thumbnails={thumbnails} positions={[15,16,17]} height={248} marginBottom={33} />
-				<Thumbnails thumbnails={thumbnails} positions={[18,19,20]} height={248} marginBottom={33} />
+				<Thumbnails thumbnails={thumbnails} positions={[13,14,15]} height={248} marginBottom={33} />
+				<Thumbnails thumbnails={thumbnails} positions={[16,17,18]} height={248} marginBottom={33} />
+				<Thumbnails thumbnails={thumbnails} positions={[19,20,21]} height={248} marginBottom={33} />
 				<p>
 				At one point in time, <em>Candy</em> made the switch from a 2D to a 3D map. Now, episode art was shown more elaborately near the player's progress, 
 				rather than flat images behind the game board. 
@@ -170,8 +171,8 @@ const CandyCrushSaga = (props) => {
 				It was an exciting challenge to both rework our old episodes into this new format and then see
 				where the opportunities for new ways of showing stories could take us.
 				</p>
-				<Thumbnails thumbnails={thumbnails} positions={[21,22,23]} height={298} marginBottom={33} />
-				<Thumbnails thumbnails={thumbnails} positions={[24,25,26]} height={298} marginBottom={33} />
+				<Thumbnails thumbnails={thumbnails} positions={[22,23,24]} height={298} marginBottom={33} />
+				<Thumbnails thumbnails={thumbnails} positions={[25,26,27]} height={298} marginBottom={33} />
 				<p>
 				For each episode, the team would brainstorm narrative and titles themselves, no matter how the look and feel evolded
 				over the years. 
@@ -180,25 +181,25 @@ const CandyCrushSaga = (props) => {
 				our ways of working continuously as well, tackling unique challenges like maintaining a decent app size for a game that's filling up 
 				with years worth of content!
 				</p>
-				<Thumbnails thumbnails={thumbnails} positions={[27,28,29]} height={298} marginBottom={33} />
-				<Thumbnails thumbnails={thumbnails} positions={[30,31,32]} height={298} marginBottom={33} />
+				<Thumbnails thumbnails={thumbnails} positions={[28,29,30]} height={298} marginBottom={33} />
+				<Thumbnails thumbnails={thumbnails} positions={[31,32,33]} height={298} marginBottom={33} />
 				<p>
 				I have eventually created over 50 episodes and helped out with dozens more.<br /><br />
 				Needless to say, it has been really special being a part of a live game for so long. 
 				We celebrated many milestones in mobile gaming together, with an example shown below!
 				</p>
-				<Video videoId={"vU9W3EWRrHQ"} />
+				<Video videoId={"vU9W3EWRrHQ"} hasThumbnail={'1OYFDlb7lbOwoTRmF49CgA_b-mG6g2t9I'} />
 				<p>
 				One great milestone was reaching level 2000 at the time, around which King also launched a great campaign. I got to work on the episode Bubblegum Bazaar, 
 				featuring this special level.<br /><br /> 
 				I depicted main characters Tiffi and Yeti in search what would happen after level 2000. The mischevious Bubblegum Troll was there to soothe their Y2K scare
 				with a glimpse into the future.
 				</p>
-				<Thumbnails thumbnails={thumbnails} positions={[33,34,35]} height={248} marginBottom={0} />
+				<Thumbnails thumbnails={thumbnails} positions={[34,35,36]} height={248} marginBottom={0} />
 				<p>
 				These designs were featured on worldwide media (such as {BBC_URL}), in app stores and on social media. 
 				</p>
-				<Video videoId={"D4pCVx6ln3E"} />
+				<Video videoId={"D4pCVx6ln3E"} hasThumbnail={'1raXQnJo5wJvxvaIN-JWQJYJ2NkMbPrP8'} />
 				<p>
 				Another great memory was Candy’s first collaboration with an outside brand.<br /><br />
 				Creative Director Nina Garcia of the US based magazine Marie Claire worked with King to design a <em>Candy</em> level herself. 
@@ -209,19 +210,19 @@ const CandyCrushSaga = (props) => {
 				<p>
 				I was part of the team working on this temporary feature by designing both the office setting and of course a Candified version of Nina Garcia!
 				</p>
-				<Video videoId={"xVzDu_g8-vY"} hasThumbnail={'1OYFDlb7lbOwoTRmF49CgA_b-mG6g2t9I'}/>
+				<Video videoId={"xVzDu_g8-vY"} hasThumbnail={'1cKa0YUHFQw-M5uI9FJrVBsm4uuVQXQNX'}/>
 				<p>
 				Behind the scenes, I have done my best to support the art function in different ways. I've done quite some in-house presentations, 
 				hosted workshops centered around learning and ways of working and had a lot of fun organizing offsites and other events to promote team bonding.
 				</p>
-				<Thumbnails thumbnails={thumbnails} positions={[36,36,36]} height={248} marginBottom={0} />
+				<Thumbnails thumbnails={thumbnails} positions={[37,37,37]} height={248} marginBottom={0} />
 				<p>
 				Additional significant learning opportunities were, among others, attending in-house courses centered on management and leadership, 
 				life drawing seminars from Samantha Youssef, sculpting by Andrea Blasich and VFX by Francisco García-Obledo. <br /><br />
 				Over the years I also represented 
 				King at distinguished art conferences like THU and the IAMAG Master Classes.
 				</p>
-				<Thumbnails thumbnails={thumbnails} positions={[36,36,36]} height={248} marginBottom={0} />
+				<Thumbnails thumbnails={thumbnails} positions={[37,37,37]} height={248} marginBottom={0} />
 				<p>
 				Outside of working with the game team and art craft, I have had many side projects supporting King as a company. I have created 
 				graphics for internal use such as event invitations, office maps, portraits of players and colleagues and more. 
@@ -245,7 +246,7 @@ const CandyCrushSaga = (props) => {
 				participation in the video thanking the fans for playing all the way to 5000 levels.<br /><br />
 				That being said - new levels are still coming every week!
 				</p>
-				<Video videoId={"LXJmCvBNxUE"} />
+				<Video videoId={"LXJmCvBNxUE"} hasThumbnail={'18_eFhti2yERellr0GiAiJBT5XBJdLmi6'} />
 			</div>
 		</Fragment>
 		)
