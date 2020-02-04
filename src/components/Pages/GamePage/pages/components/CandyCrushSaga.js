d36c6430a483a2583f5976a01dfcb53cfe98b136
JoeyM <joeytermeulen@gmail.com>
2020-02-04 19:34:32 +0100

    finished Candy page

--- src/components/Pages/GamePage/pages/components/CandyCrushSaga.js
+++ src/components/Pages/GamePage/pages/components/CandyCrushSaga.js
@@ -54,13 +54,16 @@ const CandyCrushSaga = (props) => {
 		{image: '1SXbIVOORTr0cEe7fBdG33wO9Ay_L2MGH', text: 'Didi the dragon', options: 'whiteBG'}, //3 Didi
 		{image: '1nJ9tLv59UgC5rhOLFqELfdaqrkR5VHgR', text: 'Encountering a Pet capsule on the level pin'}, //4 CandyPet_1
 		{image: '1d6L4qCdormqiZIsTu6rZU6GSx1nvT2_L', text: 'Collect candies in-game to bake treats'}, //5 CandyPet_2
-		{image: '1aiBu9T10VQVgN1eiXhX959SR1GAzJxHL', text: 'Didi featured a lot across all types of media'}, //6 CCandyPet_3
-		{image: '1-SQnVu1RhwijzPjxEIjmLR2oPAEGjbuT', text: 'Collect candies in-game to bake treats'}, //7 CandyPet_Screenshot
-		{image: '1xGSHFkDQFpTMKoHy6ugKN8TYqIfx4p35', text: 'Didi featured a lot across all types of media'}, //8 CandyPet_Featuring
-		{image: '1-DmS4T4Y0lo_E7hZkGY0YBDvcRCkiDIl', text: 'Pet Rami, based on my remake of existing character Ramsey', options: 'isMedium' }, //9 CandyPet_Rami
+		{image: '1aiBu9T10VQVgN1eiXhX959SR1GAzJxHL', text: 'Didi featured a lot across all types of media'}, //6 CandyPet_3
+		{image: '1QGcxyEEq96QnfWF10mZeOaqQU-WGCwML', text: ''}, //7 CCS_SnappysGarden1
+		{image: '1r1YgRktTWdAIlbHSa3zB9DtbHK7V1Mrw', text: "Mr. Snappy's Garden"}, //8 CCS_SnappysGarden2
+		{image: '1Ua6wFDyi1Q5XalOyBR5Z_tyPcKcks6rt', text: ''}, //9 CCS_SnappysGarden3
 		{image: '1aqAdhD0XZiCUMXxLT2Lw280vV9AiqfkW', text: "New Year's 2015-2016"}, //10 LiveOps_NY2016
 		{image: '1ESIefIwsO-BeQAjd4LygQx_Km_QE3tiV', text: "Yeti's map treasure hunt"}, //11 LiveOps_Keys
 		{image: '1edi684aIhpwKzKSl1Jt7nNxe2NM4_Hx1', text: 'Halloween 2017'}, //12 LiveOps_Spooky
+		{image: '1-efSeFOtDJ84sBBlCXpaL4Wg3f09q2Dn', text: 'Experimenting with tiling vector art for the Beanstalk Challenge'}, //13 CCS_Beanstalk1
+		{image: '19o9rD9FoeCj6sQpuIhibbEgextHvQkju', text: 'Working on the look and feel with custom outfits for Tiffi'}, //14 CCS_Beanstalk2
+		{image: '1vS0CCvAvoK-KjmNTUQILAC3VoXpR3d1m', text: 'A shot of the final stalk growing in-game'}, //15 CCS_Beanstalk3
 		{image: '1aoX-_eAYLRZY9hB9QPsySBf55Sl03KTO', text: 'Crispy Clearing'}, //13 EP1
 		{image: '1RAeVOhoDtv3OTJ_ns4mV__QILSdyPKbc', text: 'Treacle Retreat'}, //14 EP2
 		{image: '1RhkKcagVgcqH-Tjtg1fguLihN8KYdh5r', text: 'Cobweb Canopy'}, //15 EP3
@@ -152,22 +155,24 @@ const CandyCrushSaga = (props) => {
 				</p>
 				<BigImage googleId={"1WvdHr4wYMs0Zurtm_IvVlNp4dhYUtIdR"} />
 				<p>
-				Other live events I participated in producing were the aforementioned Booster Garden, the Beanstalk Challenge, Yeti's Lost Keys and a challenge for New Year's Eve 2015.
+				Other live events I participated in producing were Mr. Snappy's Garden, the Beanstalk Challenge, Yeti's Lost Keys and a challenge for New Year's Eve 2015.<br /><br />
+				Mr. Snappy's Garden especially was a fun challenge, as the small size of the team producing it meant ample opportunity to be involved with the game design.
 				</p>
-				<Thumbnails thumbnails={thumbnails} positions={[7,8,9]} height={438} marginBottom={50} />
+				<Thumbnails thumbnails={thumbnails} positions={[7,8,9]} height={337} marginBottom={33} />
 				<p>
-				When working with these features, I contributed visually, with game design suggestions and with technical implementation. Sometimes I simply reskinned existing
-				features as well.
+				Developing these timed event features was always fun as it involved technical implementation of the art, combined with animation and slight VFX work.<br /><br /> 
+				That being said, sometimes existing features were simply reskinned to offer the player more relevant experiences.
 				</p>
 				<Thumbnails thumbnails={thumbnails} positions={[10,11,12]} height={374} marginBottom={33} />
+				<Thumbnails thumbnails={thumbnails} positions={[13,14,15]} height={256} marginBottom={50} />
 				<p>
 				One of my biggest occupations within <em>Candy</em>, however, has been involvement with the Saga map format and its episodes. <br /><br />
 				Episodes are bundles of 15 new levels, released every week, with custom art and narrative on the Saga map and behind the game board. 
 				Here are some examples of mine!
 				</p>
-				<Thumbnails thumbnails={thumbnails} positions={[13,14,15]} height={248} marginBottom={33} />
 				<Thumbnails thumbnails={thumbnails} positions={[16,17,18]} height={248} marginBottom={33} />
 				<Thumbnails thumbnails={thumbnails} positions={[19,20,21]} height={248} marginBottom={33} />
+				<Thumbnails thumbnails={thumbnails} positions={[22,23,24]} height={248} marginBottom={33} />
 				<p>
 				At one point in time, <em>Candy</em> made the switch from a 2D to a 3D map. Now, episode art was shown more elaborately near the player's progress, 
 				rather than flat images behind the game board. 
@@ -175,8 +180,8 @@ const CandyCrushSaga = (props) => {
 				It was an exciting challenge to both rework our old episodes into this new format and then see
 				where the opportunities for new ways of showing stories could take us.
 				</p>
-				<Thumbnails thumbnails={thumbnails} positions={[22,23,24]} height={298} marginBottom={33} />
 				<Thumbnails thumbnails={thumbnails} positions={[25,26,27]} height={298} marginBottom={33} />
+				<Thumbnails thumbnails={thumbnails} positions={[28,29,30]} height={298} marginBottom={33} />
 				<p>
 				For each episode, the team would brainstorm narrative and titles themselves, no matter how the look and feel evolded
 				over the years. 
@@ -185,8 +190,8 @@ const CandyCrushSaga = (props) => {
 				our ways of working continuously as well, tackling unique challenges like maintaining a decent app size for a game that's filling up 
 				with years worth of content!
 				</p>
-				<Thumbnails thumbnails={thumbnails} positions={[28,29,30]} height={298} marginBottom={33} />
 				<Thumbnails thumbnails={thumbnails} positions={[31,32,33]} height={298} marginBottom={33} />
+				<Thumbnails thumbnails={thumbnails} positions={[34,35,36]} height={298} marginBottom={33} />
 				<p>
 				I have eventually created over 50 episodes and helped out with dozens more.<br /><br />
 				Needless to say, it has been really special being a part of a live game for so long. 
@@ -199,7 +204,7 @@ const CandyCrushSaga = (props) => {
 				I depicted main characters Tiffi and Yeti in search what would happen after level 2000. The mischevious Bubblegum Troll was there to soothe their Y2K scare
 				with a glimpse into the future.
 				</p>
-				<Thumbnails thumbnails={thumbnails} positions={[34,35,36]} height={248} marginBottom={0} />
+				<Thumbnails thumbnails={thumbnails} positions={[37,38,39]} height={248} marginBottom={0} />
 				<p>
 				These designs were featured on worldwide media (such as {BBC_URL}), in app stores and on social media. 
 				</p>
@@ -226,7 +231,7 @@ const CandyCrushSaga = (props) => {
 				Over the years I also represented 
 				King at distinguished art conferences like THU and the IAMAG Master Classes.
 				</p>
-				<BigImage googleId={"1JWieYZ7l9H2NEtpNfJijoJ9_Ph6StTeu"} text={"Fitting custom outfits on existing characters"} />
+				<BigImage googleId={"1JWieYZ7l9H2NEtpNfJijoJ9_Ph6StTeu"} text={"Fitting custom outfits on pre-existing characters"} />
 				<p>
 				Outside of working with the game team and art craft, I have had many side projects supporting King as a company. I have created 
 				graphics for internal use such as event invitations, office maps, portraits of players and colleagues and more. 
